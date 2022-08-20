af07d4e4b3d1b11ff2b3d7bfa29789ec5f502a59
Garrett Gordon <gagordon@mit.edu>
2022-08-20 15:19:42 -0400

    production version

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import os
 load_dotenv()
 
 
-DEVELOPMENT = True
+DEVELOPMENT = False
 
 if DEVELOPMENT:
     SIDECHAIN_BASE_URL = "http://localhost:3000"
     METADATA_SERVER = "http://localhost:8080"
 else:
-    SIDECHAIN_BASE_URL = ""
-    METADATA_SERVER = ""
+    SIDECHAIN_BASE_URL = "http://localhost:3000"
+    METADATA_SERVER = "http://18.212.228.207/8080"
 
 AWS_S3_BUCKET_ADDRESS = os.getenv("AWS_BUCKET_ADDRESS")
 AWS_REGION = os.getenv("AWS_DEFAULT_REGION")
