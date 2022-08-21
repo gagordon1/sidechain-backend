8bda7302acc91edf5e0b4847fe916d24115f199e
Garrett Gordon <gagordon@mit.edu>
2022-08-21 14:57:00 -0400

    switching to sidechain rinkeby database

--- config.py
+++ config.py
@@ -8,11 +8,12 @@ DEVELOPMENT = False
 if DEVELOPMENT:
     SIDECHAIN_BASE_URL = "http://localhost:3000"
     METADATA_SERVER = "http://localhost:8080"
+    AWS_METADATA_TABLE_NAME = "Sidechain"
 else:
     SIDECHAIN_BASE_URL = "http://localhost:3000"
     METADATA_SERVER = "http://18.212.228.207:8080"
+    AWS_METADATA_TABLE_NAME = "Sidechain-Rinkeby"
 
 AWS_S3_BUCKET_ADDRESS = os.getenv("AWS_BUCKET_ADDRESS")
 AWS_REGION = os.getenv("AWS_DEFAULT_REGION")
 AWS_BUCKET = os.getenv("BUCKET_NAME")
-AWS_METADATA_TABLE_NAME = "Sidechain"
